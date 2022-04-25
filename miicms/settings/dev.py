c46698baab22165655aec1a91b6ae5606ca689af
tienv2i <tienv2i@gmail.com>
2022-04-25 19:23:31 +0700

    First commit

---
+++ miicms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-q!2fzm7p&*9-%0jk%j=txnp52_s$#pqt5k$@=6(+9h2d*430np'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
